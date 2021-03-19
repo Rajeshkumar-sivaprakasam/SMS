a37ee63cacbcf175c36136e1df9292d85e37e9a8
Rajeshkuar Sivapraksam <rk.sivaprakasam@outlook.com>
2021-03-19 11:29:29 +0530

    Functionality added to Registration!

--- SMS/Controllers/BatchController.cs
+++ SMS/Controllers/BatchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +9,98 @@ namespace SMS.Controllers
 {
     public class BatchController : Controller
     {
+        private Models.AppContext _context;
+        public BatchController()
+        {
+            _context = new Models.AppContext();
+        }
         // GET: Batch
         public ActionResult Index()
         {
-            return View();
+            var list = _context.Batches.ToList();
+            return View(list);
         }
-
         public ActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Batch batchObj)
+        {
+            if (batchObj != null)
+            {
+                if (batchObj.Id > 0)
+                {
+
+                    var batchFromDb = _context.Batches.FirstOrDefault(u => u.Id == batchObj.Id);
+                    if (batchFromDb == null)
+                        return HttpNotFound();
+                    batchFromDb.BatchName = batchObj.BatchName;
+                    batchFromDb.Year = batchObj.Year;
+
+                }
+                else
+                {
+                    _context.Batches.Add(batchObj);
+                }
+            }
+            _context.SaveChanges();
+            //return View("~/Views/Student/GetStudent.cshtml");
+            return RedirectToAction("Index", "Batch");
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var batchDetails = _context.Batches.Where(x => x.Id == id).FirstOrDefault();
+            if (batchDetails != null)
+            {
+                return View("create", batchDetails);
+            }
+            return View();
+        }
+        /*public ActionResult Details()
+        {
+            return View();
+        }
+        */
+
+        public ActionResult Details(int? id)
+        {
+            //value types and reference type
+
+
+
+            var batchDetails = _context.Batches.Where(x => x.Id == id).FirstOrDefault();
+            if (batchDetails != null)
+            {
+                return View(batchDetails);
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Delete(int? id)
+        {
+
+            if (id == null)
+                return HttpNotFound();
+
+            var batch = _context.Batches.FirstOrDefault(u => u.Id == id);
+
+            if (batch == null)
+                return HttpNotFound();
+            _context.Batches.Remove(batch);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
         }
     }
 }
--- SMS/Controllers/RegistrationController.cs
+++ SMS/Controllers/RegistrationController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +9,87 @@ namespace SMS.Controllers
 {
     public class RegistrationController : Controller
     {
+        private Models.AppContext _context;
+        public RegistrationController()
+        {
+            _context = new Models.AppContext();
+        }
         // GET: Registration
         public ActionResult Index()
         {
-            return View();
+            var list = _context.Registrations.ToList();
+            return View(list);
         }
         public ActionResult Create()
         {
+
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(Registration regObj)
+        {
+            if (regObj != null)
+            {
+                if (regObj.Id > 0)
+                {
+
+                    var regFromDb = _context.Registrations.FirstOrDefault(u => u.Id == regObj.Id);
+                    if (regFromDb == null)
+                        return HttpNotFound();
+                    regFromDb.FirstName = regObj.FirstName;
+                    regFromDb.LastName = regObj.LastName;
+                    regFromDb.Batch = regObj.Batch;
+                    regFromDb.Course = regObj.Course;
+                    regFromDb.Email = regObj.Email;
+                }
+                else
+                {
+                    _context.Registrations.Add(regObj);
+                }
+            }
+
+            _context.SaveChanges();
+            //return View("~/Views/Student/GetStudent.cshtml");
+            return RedirectToAction("Index", "Registration");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var regDetails = _context.Registrations.Where(r => r.Id == id).FirstOrDefault();
+            if(regDetails != null)
+            {
+                return View("create", regDetails);
+            }
+            return View();
+        }
+
+        public  ActionResult Details(int? id)
+        {
+            var regDetails = _context.Registrations.Where(r => r.Id == id).FirstOrDefault();
+
+            if(regDetails != null)
+            {
+                return View(regDetails);
+            }
+            return View();
+        }
+
+        public  ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+
+            var regDetails = _context.Registrations.Where(r => r.Id == id).FirstOrDefault();
+            if (regDetails == null)
+                return HttpNotFound();
+            _context.Registrations.Remove(regDetails);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+
     }
 }
